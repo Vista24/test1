ee2ba5977ad45e608ce7d53a461657a04913daa4
Vista24 <gasic989@gmail.com>
2025-11-08 17:57:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,14 +11,23 @@ game = True
 player = Rect(0,0,100,100)
 
 big_rock = transform.scale(image.load("big_rock.png"),(100,100))
-
+full_g_block = transform.scale(image.load("full_g_block.png"),(250,100))
+full_g_block = transform.scale(image.load("full_g_block.png"),(250,100))
+full_g_block = transform.scale(image.load("full_g_block.png"),(250,100))
+full_g_block = transform.scale(image.load("full_g_block.png"),(250,100))
+full_h_block = transform.scale(image.load("full_h_block.png"),(100,300))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
     window.fill((0,0,0))
     draw.rect(window,(255,0,0),player) #
-    window.blit(big_rock,(100,100)) #
+    window.blit(big_rock,(100,310)) #
+    window.blit(full_g_block,(10,400)) #
+    window.blit(full_g_block,(200,400)) #
+    window.blit(full_g_block,(400,400)) #
+    window.blit(full_g_block,(500,400)) #
+    window.blit(full_h_block,(-10,200)) #
     keys = key.get_pressed()
     if keys[K_w]:
         player.y -= 5
